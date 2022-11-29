98ced4a9042d122e02b127be1b67fb361c123d1a
jsp-KW <70892401+jsp-KW@users.noreply.github.com>
2022-11-29 19:39:42 +0900

    Delete 텔레그램챗봇 directory

--- 텔레그램챗봇/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN','')
-CHAT_ID = os.environ.get('CHAT_ID','')
-
-
-if not TELEGRAM_TOKEN or not CHAT_ID:
-    raise Exception ('TELEGRAM_TOKEN, CHAT_ID 확인 필요')
